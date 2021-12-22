bd62eb4acb22a76c824d88f947e882b169a4ee40
KenAdams777 <andrey.kalyshenko@gmail.com>
2021-12-22 16:50:12 +0200

    Updates

---
+++ precache-manifest.7126539bd06cc66cf62328a25af931c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d3e0386c1c9efebff1396da0b25de6",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "494af0a78a6a0c3e6dc3",
+    "url": "/Quiz/static/css/main.9a3dbded.chunk.css"
+  },
+  {
+    "revision": "89612fd9ef158d8ab1df",
+    "url": "/Quiz/static/js/2.7cd6acd5.chunk.js"
+  },
+  {
+    "revision": "0f16022b59faaacb4bcaf716874b0171",
+    "url": "/Quiz/static/js/2.7cd6acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494af0a78a6a0c3e6dc3",
+    "url": "/Quiz/static/js/main.dd13f905.chunk.js"
+  },
+  {
+    "revision": "06b9bc788f3ef789891b",
+    "url": "/Quiz/static/js/runtime-main.8bc1fe9f.js"
+  }
+]);
